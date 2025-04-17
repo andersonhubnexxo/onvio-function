4014a0e80782e5a3a4a42fc9b424118dff08a811
andersonhubnexxo <anderson.santana@hubnexxo.com.br>
2025-04-17 13:55:49 -0300

    Update __init__.py

--- HttpTriggerOnvio/__init__.py
+++ HttpTriggerOnvio/__init__.py
@@ -7,7 +7,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Executando função: HttpTriggerOnvio")
 
     # Substitua essa URL pelo caminho correto do seu token.txt no SharePoint
-    token_url = "https://hubnexxo.sharepoint.com/sites/.../token.txt"
+    token_url = "https://hubnexxo-my.sharepoint.com/:t:/g/personal/anderson_santana_hubnexxo_com_br/ERyPNOZAYTRPiEj1NAV8GCAByju3EZp-Fw54KzzedptMCw?e=Uo4E6O"
 
     try:
         token_resp = requests.get(token_url)
